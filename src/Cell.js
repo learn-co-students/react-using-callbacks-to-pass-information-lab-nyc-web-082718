3cc4fcc23631d4971c522e178544668e77ca010a
yapjinai <jinai.jyap@gmail.com>
2018-10-29 14:23:58 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,30 @@
+// find a way to access the currently selected color from Matrix's state without passing the value explicitly as a prop. (Consider passing a method from Matrix to Cell instead. You will want to create an additional method in Matrix to do this.)
+// implement a handleClick method that that updates the Cell's state with the current selected color
+
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+
+  handleClick = (event) => {
+    console.log('clicking on a cell');
+    // console.log(this.state.colorMethod);
+    this.setState({
+      color: this.props.colorMethod()
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
       </div>
     )
   }
-  
+
 }
--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -1,13 +1,21 @@
+// update makeColorSwatches to provide the <div>s it is rendering with some way to fire off that method we wrote for Matrix (hint: consider using an event listener and controlling the argument when an event handler is fired)
+
 import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
-  
+
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={this.chooseColor}/>
     })
   )
-  
+
+  chooseColor = (event) => {
+    const newColor = event.target.style.backgroundColor
+    this.props.selector(newColor)
+    // console.log(this.props);
+  }
+
   render() {
     return (
       <div id="colorSelector">
@@ -15,5 +23,5 @@ export default class ColorSelector extends Component {
       </div>
     )
   }
-  
+
 }

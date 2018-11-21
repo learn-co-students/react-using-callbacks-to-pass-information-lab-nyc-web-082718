de2c117ade464e2375297d584e46fe5952ef788f
github username <github email address>
2018-11-21 18:11:05 -0500

    Done, but still failing the test.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,33 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      colorSelected: ''
+    }
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} getSelectedColor={this.getSelectedColor} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  selectedColor = (event) => {
+    // console.log(color.target.style.backgroundColor);
+    this.setState({colorSelected: event.target.style.backgroundColor})
+  }
+
+  getSelectedColor = () => {
+    return this.state.colorSelected
+  }
+
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector selectedColor={this.selectedColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
